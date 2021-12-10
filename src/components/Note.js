6983b44c42219447c596d9a7bde8c85d4434f96c
Andrey Drozd <drozdandrew58@gmail.com>
2021-12-10 17:42:38 +0300

    refactor: formatting the code and filter imports

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,92 +1,24 @@
-import { useEffect, useState } from "react";
-import notesData from "../notesData";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-import { useStyles } from "../styles/styles";
-import ActiveNoteDisplay from "./ActiveNoteDisplay";
+import { useState } from "react";
 
-const Note = (props) => {
-  const classes = useStyles(props);
-  const [activeItem, setActiveItem] = useState(null);
+import NOTES from "../notesData";
+import CardNote from './CardNote'
+import ActiveNote from "./ActiveNoteBlock";
 
-  const PostNote = notesData.map((post) => {
-    return (
-      <ItemsNote
-        post={post}
-        setActiveItem={setActiveItem}
-        activeItem={activeItem}
-      />
-    );
-  });
+export default function Note() {
+  const [activeNote, setActiveNote] = useState(null);
 
   return (
-    <div className={classes.root}>
-      {!activeItem && <ActiveNoteDisplay />}
-      {PostNote}
-    </div>
-  );
-};
-
-const ItemsNote = ({
-  post: { id, title, description, date },
-  activeItem,
-  setActiveItem,
-}) => {
-  const classes = useStyles();
-  const [color, setColor] = useState(classes.noteColor);
-  const [display, setDisplay] = useState([]);
-
-  useEffect(() => {
-    if (activeItem !== id) {
-      setColor(classes.noteColor);
-      setDisplay(notesData);
+    <div>
+      {!activeNote && <ActiveNote />}
+      {NOTES.map((note) => (
+        <CardNote
+          note={note}
+          setActiveNote={setActiveNote}
+          activeNote={activeNote}
+        />
+      )
+    )
     }
-  }, [activeItem, id]);
-
-  const handleColor = () => {
-    setColor(classes.activeColor);
-    setActiveItem(id);
-  };
-
-  const handleToggleClick = (id) => {
-    setDisplay((display) =>
-      display.includes(id)
-        ? display.filter((entry) => entry !== id)
-        : [...display, id]
-    );
-  };
-
-  return (
-    <Card
-      className={classes.root}
-      key={id}
-      onClick={() => handleColor(classes.activeColor)}
-      elevation={3}
-    >
-      <CardContent
-        className="notes-wrapper"
-        onClick={() => handleToggleClick(notesData.id)}
-      >
-        <CardContent className={color}>
-          <Typography variant="h5">{title}</Typography>
-          <Typography variant="subtitle2">
-            {description.substring(0, 20)}...
-          </Typography>
-          <div>{date}</div>
-        </CardContent>
-      </CardContent>
-
-      <CardContent>
-        {display.includes(notesData.id) && (
-          <CardContent className="display-wrapper" key={notesData.id}>
-            <Typography variant="h5">{title}</Typography>
-            <Typography variant="body2">{description}</Typography>
-            <div>{date}</div>
-          </CardContent>
-        )}
-      </CardContent>
-    </Card>
-  );
+    </div>
+  )
 };
-export default Note;
