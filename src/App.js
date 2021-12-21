33e1d175dd3f0265c779055c97d8da2328401a41
Andrey Drozd <drozdandrew58@gmail.com>
2021-12-22 00:23:04 +0300

    refactor: moved the styled component to a separate file and added/deleted imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from "react";
-import Note from './components/Note'
-import {styled} from '@mui/system';
-
-const AppBlock = styled('div')({
-    maxWidth: 500,
-    margin: "0 auto",
-    boxSizing: "border-box",
-  })
+import Note from './components/Note';
+import {AppBlock} from './/styled';
 
 export default function App () {
     return(
