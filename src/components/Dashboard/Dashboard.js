a82dc16e440936c53f1570c1d46e15d2ee635daf
Andrey Drozd <drozdandrew58@gmail.com>
2021-12-28 21:47:52 +0300

    feat: added new component

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ItemsContainer from "../ItemsContainer/ItemsContainer";
+
+const Dashboard = () => {
+  return <ItemsContainer />;
+};
+
+export default Dashboard;
