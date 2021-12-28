2ce37b5708317a6a2cf785c74e5553e3f364d697
Andrey Drozd <drozdandrew58@gmail.com>
2021-12-28 21:43:12 +0300

    refactor: added new style

--- src/styled.js
+++ src/styled.js
@@ -1,19 +1,22 @@
-import {styled} from "@mui/system";
+import { styled } from "@mui/system";
 
-export const AppBlock = styled('div')({
-    maxWidth: 500,
-    margin: "0 auto",
-    boxSizing: "border-box",
-})
+export const AppBlock = styled("div")({
+  maxWidth: 500,
+  margin: "0 auto",
+  boxSizing: "border-box",
+});
 
-export const ActiveNoteBlock = styled("h3")({
-    color:  "#00BFFF",
+export const ActiveNoteBlock = styled("h2")({
+  color: "#00BFFF",
 });
 
 export const CardWrapper = styled("div")({
-    marginTop: 20,
+  marginTop: 20,
 });
 
-export const CardActiveColor = styled("div")(({props}) => ({
-    color: props ? "#00BFFF" : null,
+export const CardActiveColor = styled("div")(({ active }) => ({
+  color: active ? "#00BFFF" : "#000000",
+  "&:hover": {
+    cursor: "pointer",
+  },
 }));
