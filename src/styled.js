1a19c8f52027e5d8a1f53008c7311a3aaf06195f
Andrey Drozd <drozdandrew58@gmail.com>
2022-01-04 20:39:41 +0300

    feat: added styles for app

---
+++ src/styled.js
@@ -0,0 +1,27 @@
+import { styled } from "@mui/system";
+import TextField from "@mui/material/TextField";
+
+export const AppBlock = styled("div")({
+  maxWidth: 500,
+  margin: "0 auto",
+  boxSizing: "border-box",
+});
+
+export const ActiveNoteBlock = styled("h2")({
+  color: "#00BFFF",
+});
+
+export const CardWrapper = styled("div")({
+  marginTop: 20,
+});
+
+export const CardActiveColor = styled("div")(({ active }) => ({
+  color: active ? "#00BFFF" : "#000000",
+  "&:hover": {
+    cursor: "pointer",
+  },
+}));
+
+export const StyledTextField = styled(TextField)({
+  padding: 5,
+})
