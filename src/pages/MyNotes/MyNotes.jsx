3bb4334f22c3c96bb6e0fd4d367386a4b6d65d2d
Andrey Drozd <drozdandrew58@gmail.com>
2022-01-25 23:37:28 +0300

    feat: add My notes page

---
+++ src/pages/MyNotes/MyNotes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Notes from "pages/Notes/Notes"; 
+
+const MyNotes = () => (
+
+    <>
+        <h1>My Notes Page</h1>
+        <div><Notes/></div>
+    </>
+)
+
+export default MyNotes;
