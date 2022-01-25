8d1970829c6b7b86e5931e4ec486ef549afdff7f
Andrey Drozd <drozdandrew58@gmail.com>
2022-01-25 23:37:51 +0300

    feat: add not found page

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import {Link } from "react-router-dom";
+// import Button from "@mui/material/Button";
+// import {StyledButtonLink} from './styled'
+
+const NotFound = () => (
+  <>
+    <h1>404 - Page not found</h1>
+  </>
+);
+
+export default NotFound;
