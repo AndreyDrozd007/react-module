d7f6d4791bc9e113012c14a24c4186d312edab4c
Andrey Drozd <drozdandrew58@gmail.com>
2022-01-25 23:38:11 +0300

    feat: add Shared notes page

---
+++ src/pages/SharedNotes/SharedNotes.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Notes from "pages/Notes/Notes"; 
+
+const SharedNotes = () => (
+    <>
+        <h1>Shared notes Page</h1>
+        <div><Notes/></div>
+    </>
+)
+
+export default SharedNotes;
