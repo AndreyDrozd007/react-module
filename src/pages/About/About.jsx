0f27b73d240daae064b1dd9df6a76c39bdc64b76
Andrey Drozd <drozdandrew58@gmail.com>
2022-01-25 23:36:31 +0300

    feat: add About page

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => (
+    <>
+        <h1> About Page</h1>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation 
+        ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+    </>
+
+)
+
+export default About;
