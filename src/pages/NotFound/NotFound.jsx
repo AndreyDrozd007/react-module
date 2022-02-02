40886d7222ab8694d0071109d4251df766925081
Andrey Drozd <drozdandrew58@gmail.com>
2022-02-02 17:04:15 +0300

    refactor: move tab to the common const and add function map for it

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import LinkTab from "components/LinkTab/LinkTab";
+import { ROUTES } from "config/constants";
 
-const NotFound = () => (
-  <>
-    <h1>404 - Page not found</h1>
-    <div>
-      <LinkTab
-        label="Go back the main Page"
-        pathname="/notes"
-        />
-    </div>
-  </>
-);
+const NotFound = () => {
+  const GoBackTab = ROUTES[2].map(({ element, id, pathname, label }, tab) => (
+    <LinkTab
+      element={element}
+      key={id}
+      pathname={pathname}
+      label={label}
+      tab={tab}
+    />
+  ));
+
+  return (
+    <>
+      <h1>404 - Page not found</h1>
+      <div>
+        {GoBackTab}
+      </div>
+    </>
+  );
+};
 
 export default NotFound;
