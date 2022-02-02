a639c1dafcb4ae002a85dcc0946185dc34afd973
Andrey Drozd <drozdandrew58@gmail.com>
2022-02-02 17:00:34 +0300

    refactor: move all routes to the common const and add function map for their

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from "react";
-import { BrowserRouter,  Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AppBlock } from "./styled";
 import MainMenu from "pages/MainMenu/MainMenu";
-import MyNotes from "pages/MyNotes/MyNotes";
-import SharedNotes from "pages/SharedNotes/SharedNotes";
-import About from "pages/About/About";
-import NotFound from "pages/NotFound/NotFound";
+import { ROUTES } from "config/constants";
 
 export default function App() {
+  const RouteComponents = ROUTES[0].map(({ path, element, id }, route) => (
+    <Route path={path} element={element} route={route} key={id} />
+  ));
   return (
     <BrowserRouter>
       <AppBlock>
         <MainMenu />
-        <Routes>
-        <Route path="/" element={<Navigate to={"notes"} />} />
-        <Route path="notes" element={<MyNotes />} />
-        <Route path="shared-notes" element={<SharedNotes />} />
-        <Route path="about" element={<About />} />
-        <Route path="not-found" element={<NotFound />} />
-        <Route path="*" element={<Navigate to={"not-found"} />} />
-      </Routes>
+        <Routes>{RouteComponents}</Routes>
       </AppBlock>
     </BrowserRouter>
   );
