e4448023ccf3fccd729b5582f4e5c8b7ceca8d55
Andrey Drozd <drozdandrew58@gmail.com>
2022-02-02 17:05:54 +0300

    feat: add separate file for all const

---
+++ src/config/constants.js
@@ -0,0 +1,35 @@
+import { Navigate } from "react-router-dom";
+import MyNotes from "pages/MyNotes/MyNotes";
+import SharedNotes from "pages/SharedNotes/SharedNotes";
+import About from "pages/About/About";
+import NotFound from "pages/NotFound/NotFound";
+import Tab from "@mui/material/Tab";
+
+export const ROUTES = [
+  [
+    { path: "/", element: <Navigate to={"notes"} />, id: 0 },
+    { path: "/notes", element: <MyNotes />, id: 1 },
+    { path: "/shared-notes", element: <SharedNotes />, id: 2 },
+    { path: "/about", element: <About />, id: 3 },
+    { path: "/not-found", element: <NotFound />, id: 4 },
+    { path: "/*", element: <Navigate to={"not-found"} />, id: 5 },
+  ],
+  [
+    { pathname: "/notes", element: <Tab />, label: "My Notes", id: 0 },
+    {
+      pathname: "/shared-notes",
+      element: <Tab />,
+      label: "Shared Notes",
+      id: 1,
+    },
+    { pathname: "/about", element: <Tab />, label: "About", id: 2 },
+  ],
+  [
+    {
+      pathname: "/notes",
+      element: <Tab />,
+      label: "Go back the main Page",
+      id: 0,
+    },
+  ],
+];
