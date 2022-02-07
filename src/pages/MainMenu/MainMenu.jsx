ef8bde4c357d6fe1842aa1e11a1874becd6e9f99
Andrey Drozd <drozdandrew58@gmail.com>
2022-02-07 23:48:01 +0300

    refactor: added an array with tabs and changed their output from the array

--- src/pages/MainMenu/MainMenu.jsx
+++ src/pages/MainMenu/MainMenu.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+
 import LinkTab from "components/LinkTab/LinkTab";
-import { ROUTES } from "config/constants";
+import { NOTES, SHARED_NOTES, ABOUT } from "config/constants";
 
 const MainMenu = () => {
   const [tab, setTab] = useState(0);
@@ -9,22 +11,24 @@ const MainMenu = () => {
     setTab(newTab);
   };
 
-  const TabComponents = ROUTES[1].map(
-    ({ element, id, pathname, label}, tab) => (
-      <LinkTab
-        element={element}
-        key={id}
-        pathname={pathname}
-        label={label}
-        tab={tab}
-      />
-    )
-  );
+  const TABS = [
+    { pathname: NOTES, element: <Tab />, label: "My Notes", id: 0 },
+    { pathname: SHARED_NOTES, element: <Tab />, label: "Shared Notes", id: 1 },
+    { pathname: ABOUT, element: <Tab />, label: "About", id: 2 },
+  ];
 
   return (
     <div>
       <Tabs variant="fullWidth" value={tab} onChange={handleChange}>
-        {TabComponents}
+        {TABS.map(({ element, id, pathname, label }, tab) => (
+          <LinkTab
+            element={element}
+            key={id}
+            pathname={pathname}
+            label={label}
+            tab={tab}
+          />
+        ))}
       </Tabs>
     </div>
   );
