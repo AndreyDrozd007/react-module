9f2bdd4f0079dcb145b94d765e3707b553d372d1
Andrey Drozd <drozdandrew58@gmail.com>
2022-02-08 14:03:09 +0300

    refactor: changed access to paths

--- src/App.js
+++ src/App.js
@@ -7,36 +7,21 @@ import Notes from "pages/MyNotes/Notes";
 import SharedNotes from "pages/SharedNotes/SharedNotes";
 import About from "pages/About/About";
 import NotFound from "pages/NotFound/NotFound";
-import {
-  NOTES,
-  SHARED_NOTES,
-  ABOUT,
-  NAVIGATE_NOTES,
-  NAVIGATE_NOT_FOUND,
-  NOT_FOUND,
-} from "config/constants";
+import { ROUTES } from "config/constants";
 
 export default function App() {
-  const ROUTES = [
-    { path: NAVIGATE_NOTES, element: <Navigate to={"/notes"} />, id: 0 },
-    { path: NOTES, element: <Notes />, id: 1 },
-    { path: SHARED_NOTES, element: <SharedNotes />, id: 2 },
-    { path: ABOUT, element: <About />, id: 3 },
-    { path: NOT_FOUND, element: <NotFound />, id: 4 },
-    {
-      path: NAVIGATE_NOT_FOUND,
-      element: <Navigate to={"/not-found"} />,
-      id: 5,
-    },
-  ];
-  const RouteComponents = ROUTES.map(({ path, element, id }, route) => (
-    <Route path={path} element={element} route={route} key={id} />
-  ));
   return (
     <BrowserRouter>
       <AppBlock>
         <MainMenu />
-        <Routes>{RouteComponents}</Routes>
+        <Routes>
+          <Route path={ROUTES.NAVIGATE_NOTES} element={<Navigate to={"/notes"} />} />
+          <Route path={ROUTES.NOTES} element={<Notes />} />
+          <Route path={ROUTES.SHARED_NOTES} element={<SharedNotes />} />
+          <Route path={ROUTES.ABOUT} element={<About />} />
+          <Route path={ROUTES.NOT_FOUND} element={<NotFound />} />
+          <Route path={ROUTES.NAVIGATE_NOT_FOUND} element={<Navigate to={"/not-found"} />} />
+        </Routes>
       </AppBlock>
     </BrowserRouter>
   );
