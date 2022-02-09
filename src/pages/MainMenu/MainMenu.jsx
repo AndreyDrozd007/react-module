2d84f99f3f56aca19f5a204403d633dbf5e18c03
Andrey Drozd <drozdandrew58@gmail.com>
2022-02-09 10:02:58 +0300

    refactor: remove map functiond and add LinkTab component with pathname for each tab

--- src/pages/MainMenu/MainMenu.jsx
+++ src/pages/MainMenu/MainMenu.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
 
 import LinkTab from "components/LinkTab/LinkTab";
 import { ROUTES } from "config/constants";
@@ -11,24 +10,12 @@ const MainMenu = () => {
     setTab(newTab);
   };
 
-  const TABS = [
-    { pathname: ROUTES.NOTES, element: <Tab />, label: "My Notes", id: 0 },
-    { pathname: ROUTES.SHARED_NOTES, element: <Tab />, label: "Shared Notes", id: 1 },
-    { pathname: ROUTES.ABOUT, element: <Tab />, label: "About", id: 2 },
-  ];
-
   return (
     <div>
       <Tabs variant="fullWidth" value={tab} onChange={handleChange}>
-        {TABS.map(({ element, id, pathname, label }, tab) => (
-          <LinkTab
-            element={element}
-            key={id}
-            pathname={pathname}
-            label={label}
-            tab={tab}
-          />
-        ))}
+        <LinkTab pathname={ROUTES.NOTES} label="My Notes" />
+        <LinkTab pathname={ROUTES.SHARED_NOTES} label="Shared Notes" />
+        <LinkTab pathname={ROUTES.ABOUT} label="About" />
       </Tabs>
     </div>
   );
