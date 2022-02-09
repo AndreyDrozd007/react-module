e71565646c42553525795b23bb46eb15f2c6906b
Andrey Drozd <drozdandrew58@gmail.com>
2022-02-09 10:05:30 +0300

    refactor: change the LinkTAb output

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,25 +1,17 @@
 import React from "react";
-import Tab from "@mui/material/Tab";
 
 import LinkTab from "components/LinkTab/LinkTab";
 import { ROUTES } from "config/constants";
 
 const NotFound = () => {
-  const GoBackTab = {
-    pathname: ROUTES.NOTES,
-    element: <Tab />,
-    label: "Go back the main Page",
-  };
 
   return (
     <>
       <h1>404 - Page not found</h1>
       <div>
         <LinkTab
-          element={GoBackTab.element}
-          key={GoBackTab.id}
-          pathname={GoBackTab.pathname}
-          label={GoBackTab.label}
+          pathname={ROUTES.NOTES}
+          label="Go back the main Page"
         />
       </div>
     </>
